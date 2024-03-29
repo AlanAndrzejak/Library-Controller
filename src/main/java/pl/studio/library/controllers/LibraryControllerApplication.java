b0fcdb4294649065279d4e32fe97a5a6fc85f08e
Alan <alan.andrzejak@gmail.com>
2024-03-29 12:42:06 +0100

    Add basic project, spring and gradle configuration

---
+++ src/main/java/pl/studio/library/controllers/LibraryControllerApplication.java
@@ -0,0 +1,13 @@
+package pl.studio.library.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryControllerApplication.class, args);
+	}
+
+}
